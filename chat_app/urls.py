e78be9c5fc9a47386f09f60a247fdb1018dec14a
minikaccel <kolya.belov.2100@mailru>
2021-08-15 14:30:09 +0300

    first comm

---
+++ chat_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.urls import path
+
+from . import views
+
+app_name = 'chat_app'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<str:room_name>/', views.room, name='room'),
+]
---
+++ chat_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'chat_app/index.html')
+
+
+def room(request, room_name):
+    context = {
+        'room_name': room_name
+    }
+    return render(request, 'chat_app/room.html', context)
